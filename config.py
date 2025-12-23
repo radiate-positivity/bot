28cf0de8d7a77b69f11df138961321963e93404d
radiate-positivity <45203491+radiate-positivity@users.noreply.github.com>
2025-12-23 14:19:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.environ.get("ADMIN_ID")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден в переменных окружения.")
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID не найден в переменных окружения.")
 
 REVIEWS_CHANNEL_ID = "@your_reviews_channel"
 REVIEWS_CHANNEL_LINK = "https://t.me/your_reviews_channel"
 
 PR_SPECIALIST_USERNAME = "@username_specialist"
 PR_SPECIALIST_EMAIL = "partner@firma.com"
+
 PR_SPECIALIST_PHONE = "+1234567890"
