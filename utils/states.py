88a48a57b740627bdc1097fa6f9646a49417890c
radiate-positivity <45203491+radiate-positivity@users.noreply.github.com>
2026-01-16 14:32:24 +0400

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -4,10 +4,10 @@ class QuizStates(StatesGroup):
     waiting_for_start = State()
     publications = State()
     citations = State()
-    reviews = State()
     membership = State()
     media = State()
     awards = State()
     leadership = State()
     salary = State()
+
     waiting_for_results = State()
